e2351eb0de35b3869a19b7876bb48eee731a735a
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-17 11:53:30 +0530

    String funtion in python

---
+++ 13_day.py
@@ -0,0 +1,16 @@
+# string are immutable
+a="saurabh !!!!!!!!!!"
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rsplit("!"))
+print(a.replace("saurabh","raghav"))
+print(a.strip(" "))
+print(a.capitalize())
+
+
+str="welcome to the console!!"
+print(len(str))
+print(len(str.center(50)))
+print(str.center(50))
+print(a.count("a"))
