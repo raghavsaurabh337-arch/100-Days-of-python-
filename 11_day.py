f49e45cca8a38c1a9da2eea516b667bbf46c06a7
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-17 11:20:15 +0530

    String

---
+++ 11_day.py
@@ -0,0 +1,20 @@
+# String in python
+nam="saurabh"
+last="Raghav"
+print(nam)
+print(last)
+print(nam + " " + last)
+print("hello " + nam)
+
+multi_line='''
+This is a multi-line string.
+hye
+hllo
+kaise ho
+i am fine 
+'''
+print(multi_line)
+
+
+for char in nam:
+     print(char)
