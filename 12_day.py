7ccf641194c8d6c3b31ba723037dcef67cf40156
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-17 11:33:03 +0530

    Strings Slicing and Operations on Strings in Python

---
+++ 12_day.py
@@ -0,0 +1,10 @@
+# Strings Slicing and Operations on Strings in Python
+name="sauarbh,raghav"
+print(name[0:7])
+print(len(name))
+print(name[0:0:2])
+
+name="sauarbh","raghav"
+print(name[-3:])
+print(name[1])
+print(len(name[1]))
