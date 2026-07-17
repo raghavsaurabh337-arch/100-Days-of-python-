8533bba427c647254c0249e7a7f87d0b99899ab2
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-17 10:23:40 +0530

    #

--- 07_day.py
+++ 07_day.py
@@ -12,13 +12,3 @@ print()
 print(15//5)
 
 
-#Calculator using Python
-print()
-print("Calculator using Python")
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-print("Power:", a ** b)
-print("Division:", a / b)
-print("Floor Division:", a // b)
-print("Modulus:", a % b)
