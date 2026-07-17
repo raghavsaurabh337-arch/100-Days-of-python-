71615e83117ddb8079f475d3650c07746e9b1fdc
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-17 11:10:24 +0530

    user input

---
+++ 10_day.py
@@ -0,0 +1,11 @@
+# user input
+a = int(input("Enter the first number: "))
+b = int(input("Enter the first number: "))
+print(a+b)
+
+x=input("enter the frist number")
+y=input("enter the frist number")
+print(int(x)+int(y))
+print(int(x)-int(y))
+print(int(x)*int(y))
+print(int(x)/int(y))
