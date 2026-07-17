6314d6ccfdda4311266a9b4c712264cbc621d169
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-17 10:28:17 +0530

    Calculator using Python (Solution)

---
+++ 08_day.py
@@ -0,0 +1,13 @@
+#Calculator using Python
+a=20
+b=89
+
+print()
+print("Calculator using Python")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Power:", a ** b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
