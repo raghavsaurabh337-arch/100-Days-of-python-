9300388dc290f99ef788f4393a94471222ea3aba
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-17 10:20:12 +0530

    #Calculator using Python

---
+++ 07_day.py
@@ -0,0 +1,24 @@
+a=15
+b=7
+print("add",a+b)
+print("sub",a-b)
+print("mul",a*b)
+print("pow",a**b)
+
+print("div",a/b)
+print("floor div",a//b)
+print("mod",a%b)
+print()
+print(15//5)
+
+
+#Calculator using Python
+print()
+print("Calculator using Python")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Power:", a ** b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
