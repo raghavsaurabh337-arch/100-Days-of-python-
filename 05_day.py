e4ecaef881a35ee5fec2897778a4424713130ddb
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-17 09:55:42 +0530

    # comments ,  Escape Sequences, print statements

---
+++ 05_day.py
@@ -0,0 +1,9 @@
+# comments ,  Escape Sequences, print statements
+#comments: This is a comment
+'''
+multi line comments
+'''
+print("i am a good boy \n than this is a girl")
+print("hye",6,54,78)
+print("hye",6,54,78,sep=" ")
+print("hye",6,54,78,end="009\n")
