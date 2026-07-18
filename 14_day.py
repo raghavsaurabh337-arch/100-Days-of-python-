f9eb1da28df064593c0ef4515cdee8b5da9dc698
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-18 16:30:14 +0530

    if else

---
+++ 14_day.py
@@ -0,0 +1,53 @@
+# age=int(input("enter the age:"))
+# print(age==18)
+# print(age!=18)
+# if(age<=18):
+#      print("it is allow/ True")
+# else:
+#      print("it is not allow")
+
+# write a program 
+
+# product=int(input("enter the client product requiment"))
+# stock=int(input("enter the stock alibale"))
+
+# if(product<=stock):
+#      print("this is  Product Available",(product-stock))  
+# else:
+#      print("this is not Product Available NOW",(stock-product))        
+
+
+# # write a program
+# num=int(input("Enter a number:")) 
+# if(num>0):
+#      print("The number is positive")
+# elif(num==0):
+#      print("The number is zero")
+# else:
+#      print("The number is negative")
+
+
+# write a program 
+number=int(input("Enter a number:"))
+if(number<0):
+     print("The number is negative")
+elif(number<=10):
+     print("this is a nummber 1 to 10")          
+elif(number<=20):
+     print("this is a nummber 10 to 20")          
+elif(number<=30):
+     print("this is a nummber 20 to 30")          
+elif(number<=40):
+     print("this is a nummber 30 to 40")          
+elif(number<=50):
+     print("this is a nummber 40 to 50")          
+elif(number<=60):
+     print("this is a nummber 50 to 60")          
+elif(number<=70):
+     print("this is a nummber 70 to 80")          
+elif(number<=80):
+     print("this is a nummber 80 to 90")          
+elif(number<=90):
+     print("this is a nummber 90 to 100")    
+else:
+     print("this is a number but upper tha number 100")           
