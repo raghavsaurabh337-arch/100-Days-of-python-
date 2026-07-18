9e7b56ba1aa8c6f1f6e907a1f4d207b720129d31
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-18 17:39:43 +0530

    continue and break

---
+++ 19_day.py
@@ -0,0 +1,10 @@
+for i in range(1,34):
+     if(i==4):
+         break
+     print(i)
+     print()
+for i in range(1,34):
+     if(i==4):
+         print("value is 4 skip")
+         continue
+     print(i)
