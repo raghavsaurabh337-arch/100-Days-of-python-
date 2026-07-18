e1390910d94a39f60148b0ebcffb6385cfe6d680
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-18 18:01:16 +0530

    function

---
+++ 20_day.py
@@ -0,0 +1,19 @@
+# function in python
+def add(a,b):
+     c=a+b
+     print(c)
+
+def mean(a,b):
+     means=(1+2)/2
+     print(means) 
+
+def isgreater(a,b):
+     if(a>b):
+          print("a is greater than b",a)
+     else:
+          print(b,"b is greater than a")     
+a=34
+b=20
+add(a,b)
+mean(a,b)
+isgreater(a,b)
