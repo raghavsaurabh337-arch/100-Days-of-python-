9af79098fcef708858f153385a82fa75326a943c
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-18 17:25:48 +0530

    while loop

---
+++ 18_day.py
@@ -0,0 +1,11 @@
+i=0
+while(i<10):
+     print(i)
+     i=i+1
+num=int(input("enter the valuse first:"))
+while(num<=89):
+     num=int(input("enter the valuse second:"))
+     print(num)
+     
+print("loop finished")     
+          
