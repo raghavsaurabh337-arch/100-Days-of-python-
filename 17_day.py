0e9145721290fd0ff14579c0fe06e7a350d1fb55
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-18 17:11:45 +0530

    for looop

---
+++ 17_day.py
@@ -0,0 +1,24 @@
+name ="saurabh Raghav"
+print(len(name))
+for char in name:
+     print(char)
+     # print( char,end=", ")
+         
+     if (char == "h"):
+          print("this is last name")
+
+
+colors=["red","yellow","blue",'Apple',"Orange"]
+for color in colors:
+     print(color)
+     for i in color:
+          print(i)
+
+
+for num in (1, 2, 3, 4, 5):
+     print(num)
+for num in range(5):
+     print(num+1)
+for num in range(2,500,5):
+     print(num+1)
+
