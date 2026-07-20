b35630fe8a911026f44c763c197ed00b38a9dabd
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-20 17:15:10 +0530

    list in python

---
+++ 22_day.py
@@ -0,0 +1,20 @@
+list=[1,2,3,4]
+print(type(list))
+print(list)
+print(list[1])
+print(list.append(5))
+print(list.append("saurabh"))
+print(list)
+print(len(list))
+
+print(list[:2])
+
+
+if "saurabh" in list:
+     print("yes")
+else:
+     print("no")  
+
+for i in list :
+     print(i)       
+
