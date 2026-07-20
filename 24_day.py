c4555cabfe501e91d1d5a411fd0882a58b875be8
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-20 23:37:34 +0530

    tuple

---
+++ 24_day.py
@@ -0,0 +1,8 @@
+tuple=(1,4,7,8,"saurabh")
+tuple1=(1) # int , but single value tuple using for  , using t=(1,)
+print(tuple)
+print(tuple1)
+print(type(tuple))
+print(type(tuple1)) #<class 'int'>
+
+
