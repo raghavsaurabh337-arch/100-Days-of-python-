967a7a02672b67c814df89583a8a69d093167768
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-21 13:44:52 +0530

    fact

---
+++ 30.day.py
@@ -0,0 +1,17 @@
+def fact(n):  # 5*4*3*2*1
+     if(n==0 or n==1):
+          return 1
+     else:
+          return n*fact(n-1)
+
+print(fact(5))     
+
+'''
+
+f0=0
+f1=1
+f2=f1+f0
+f(n)=f(n-1)+f(n+2)
+'''
+def fabnocci(num):
+     pass
