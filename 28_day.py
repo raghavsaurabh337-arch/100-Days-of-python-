de6973db690257e7a9226cb43f9dbfbff51d5205
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-21 13:18:49 +0530

    f string

---
+++ 28_day.py
@@ -0,0 +1,8 @@
+latter="hye my name is {} and i am from {}" 
+name="Saurabh"
+con="india"
+print(latter.format(name, con))
+print(f"hye my name is {name} and i am from {con}")
+
+price=908.9909989
+print(f"this is {price:.3f}")
