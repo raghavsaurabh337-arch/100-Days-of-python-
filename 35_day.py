19b16582c9b293f18eb5974ba1730caffe22fb1e
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-22 23:32:37 +0530

    for Loop with  else  in Python

---
+++ 35_day.py
@@ -0,0 +1,15 @@
+# for loop and else
+for i in range(5):
+     print(i)
+else:
+     print("Loop completed successfully.")
+
+
+
+
+for i in range(5):
+     if i==4:
+          break
+     print(i)
+else:
+     print("Loop completed successfully.")
