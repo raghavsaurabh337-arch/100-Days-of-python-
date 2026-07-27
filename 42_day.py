5f972977809672fd4f38546136f442eb9e8d2aec
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-27 16:29:04 +0530

    Enumerate function

---
+++ 42_day.py
@@ -0,0 +1,18 @@
+# marks=[23,56,87,54,87,45,98]
+# index=0
+# for mark in marks:
+#      print(mark)
+#      if (index==5):
+#           print("you are awesome") 
+#      index=index+1     
+
+
+# Enumerate
+
+marks=[23,56,87,54,87,45,98]
+
+for index,mark in enumerate(marks):
+     print(mark)
+     if (index==5):
+          print("you are awesome") 
+     index=index+1  
