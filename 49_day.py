3d4e40705ad0c91595f083a1b2c6b9fd6cd41d4c
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-30 02:43:35 +0530

    file handing baise

---
+++ 49_day.py
@@ -0,0 +1,16 @@
+
+# 
+#  read a file
+# f=open('20_day.py','r')
+# text=f.read()
+# print(text)
+# f.close()
+
+# write a file
+
+# f=open('200_day.py','w')
+# f.write("hello")
+
+f=open('fie.txt','a')
+f.write("i'm saurabh")
+f.close()
