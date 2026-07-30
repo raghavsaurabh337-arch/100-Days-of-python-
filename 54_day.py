440bdc9dd2bd690e1215b12b34ed97e738cae3cf
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-31 00:39:59 +0530

    is , ==

---
+++ 54_day.py
@@ -0,0 +1,14 @@
+# 'is' vs '==' in Python 
+
+# is , location compare , == . is a values compares
+
+a=4
+b=4
+print(a is b) # exact location of object in memory
+print(a==b)
+
+
+password="raghav@123"
+confire_password="raghav@123"
+print(password is confire_password)
+print(password == confire_password)
