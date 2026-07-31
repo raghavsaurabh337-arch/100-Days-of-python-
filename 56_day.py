fdbe0c3c263e7f7fdb150755910cfe7361e454f6
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-31 08:47:04 +0530

    class and method , OOPs

---
+++ 56_day.py
@@ -0,0 +1,10 @@
+class emp:
+     name='saurabh'
+     print(name)
+emp()    
+
+
+def emp():
+     name="raghav"
+     print(name)
+emp()     
---
+++ 57_day.py
@@ -0,0 +1,16 @@
+class person:
+     name="harry"
+     work="software engineer"
+
+     def info(self):
+          print(f"{a.name} is a {a.work}" )
+
+a=person()
+a.name="saurabh"
+print(a.name)
+a.info()
+
+
+
+class student():
+     pass 
