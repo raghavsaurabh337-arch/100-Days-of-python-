37e3a5bd530147ff7d342f3f0bedb5a4c3968d0e
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-08-13 20:41:14 +0530

    constructors

--- 58_day.py
+++ 58_day.py
@@ -1,6 +1,16 @@
 #constructors
 class person:
-     name="saurabh"
-     work="Developer"
+     # name="saurabh"
+     # work="Developer"
+     def __init__(self,name,work):
+          self.name=name
+          self.work=work
+          print("Hey ")
      def info(self):
-          print("f{self.name}is a {self.work}")
+          print(f"{self.name} is a {self.work}")
+a=person("saurabh","AI") 
+b=person("sandeep","Aix") 
+a.info() 
+b.info()
+
+    
